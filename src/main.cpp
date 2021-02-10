8cef591634b189780400d11e142828be8f7943ce
Jacob Higgins <higginsjake909@gmail.com>
2021-02-10 17:09:38 -0600

    Added main section

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
 #include <Arduino.h>
 
+/* DECLARATIONS*/
+
+/* DEFINES */
+int main_fn(void);
+
+/* GLOBALS */
+
+
+int main_fn()
+{
+
+    /* Put Cube Functions here */
+    while(1)
+    {
+        Serial.println("In infinite while loop:");
+        delay(1000);
+    }
+
+    return 0;
+}
+
+
+/* ONLY USED TO GET TO MAIN AND SETUP SERIAL !!! */
 void setup() 
 {
     Serial.begin(9600);
     delay(10);
+    main_fn();
 
 }
 
 void loop() 
 {
-    Serial.println("In Loop:");
-    printf("Test /n");
+    // should never reach here...
     delay(1000);
 }
