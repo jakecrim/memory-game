a8b00e15366b20895e070da4599bee09d8783928
bmgbbr@mail.missouri.edu <bgallo98@gmail.com>
2021-02-15 17:05:30 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,16 @@ void setup()
 {
     Serial.begin(9600);
     delay(10);
+    
 
 }
 
 void loop() 
 {
-    Serial.println("In Loop:");
+    Serial.println("In Loop: hello");
     printf("Test /n");
     delay(1000);
 }
+
+void station1()
+{}
